1a297345f16d58362c86ed899068ed06d0938209
serhiy <my@localhost.met>
2017-02-21 17:04:55 +0200

    Example cpp-file

---
+++ page6Pr.cpp
@@ -0,0 +1,14 @@
+// getinf о. срр — ввод и вывод
+#include <iostream>
+int main()
+{
+using namespace std;
+int carrots;
+cout « "How many carrots do you have?" « endl;
+cin » carrots; // ввод C++
+cout << "Here are two more. ";
+carrots = carrots + 2;
+// следующая строка выполняет конкатенацию вывода
+cout << "Now you have" << carrots « " carrots." << endl;
+return 0;
+}
